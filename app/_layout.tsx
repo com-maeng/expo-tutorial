f8bb8de778af6753ad8c57573c59b0d37973b90a
Yi Hong-ju <to_hongju@pm.me>
2025-06-08 20:18:06 +0900

    Update RootLayout
    - Replaced 'index' Stack.Screen to '(tabs)' Stack.Screen
    - Removed 'about' Stack.Screen
    - Added StatusBar w. 'light' style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,18 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return(
-    <Stack>
-      <Stack.Screen name="index" options={{
-        headerTitle: "Expo app.",
-        headerLeft: () => <></>
-      }}/>
-      <Stack.Screen name="about" options={{
-        headerTitle: "About"
-      }}/>
-      <Stack.Screen name="+not-found" options={{
-        headerShown: false
-      }}/>
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{
+          headerShown: false
+        }}/>
+        <Stack.Screen name="+not-found" options={{
+          headerShown: false
+        }}/>
+      </Stack>
+      <StatusBar style="light" />
+  </>
   )
 }
